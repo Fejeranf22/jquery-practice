ec638f679159c81fe18a73d89457306bb92d75dc
Ty Fejerang <tyfejerang@Tys-MacBook.local>
2017-03-08 13:26:48 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  // $("h1").hide()
+  // $("#btn").mouseleave(function(){
+  //     $(".intro").slideToggle("slow")
+  // })
+  //
+  // $( "#clickme" ).click(function() {
+  //   $( "#book" ).animate({
+  // opacity: 0.25,
+  // left: "+=50",
+  // height: "toggle"
+  // // }, 5000, function() {
+  // // // Animation complete.
+  //   $(".btn").click(function(){
+  //     $("#newList").prepend("<li>four</li>")
+    $("#btn").click(function(){
+      $("h1").toggleClass("whatever")
+    })
+
+
+})
